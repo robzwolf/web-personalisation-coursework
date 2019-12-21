09bae2d36dc4d42d3b220e3245dc7eacb11da275
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2019-12-21 12:04:42 +0000

    Finish Forrest Gump tutorial

--- code/example.py
+++ code/example.py
@@ -1,3 +1,5 @@
+# This script follows the tutorial at https://stackabuse.com/creating-a-simple-recommender-system-in-python-using-pandas/
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -32,3 +34,20 @@ plt.rcParams['patch.force_edgecolor'] = True
 # ratings_mean_count['rating'].hist(bins=50)
 sns.jointplot(x='rating', y='rating_counts', data=ratings_mean_count, alpha=0.4)
 plt.show()
+
+user_movie_rating = movie_data.pivot_table(index='userId', columns='title', values='rating')
+print(user_movie_rating.head())
+
+forrest_gump_ratings = user_movie_rating['Forrest Gump (1994)']
+print(forrest_gump_ratings.head())
+
+movies_like_forest_gump = user_movie_rating.corrwith(forrest_gump_ratings)
+corr_forrest_gump = pd.DataFrame(movies_like_forest_gump, columns=['Correlation'])
+corr_forrest_gump.dropna(inplace=True)
+
+corr_forrest_gump = corr_forrest_gump.join(ratings_mean_count['rating_counts'])
+# print(corr_forrest_gump.sort_values('rating_counts', ascending=False).head(10))
+print(corr_forrest_gump[corr_forrest_gump['rating_counts']>50].sort_values('Correlation', ascending=False).head())
+
+# This will print out the five movies that are similar to Forrest Gump, based on the ratings of similar films
+# by the same users.
