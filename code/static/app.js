c87368907ec9f250001446723842214ced805e72
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2019-12-22 10:44:18 +0000

    Web interface to get recommendations based on user ID

--- code/static/app.js
+++ code/static/app.js
@@ -1,4 +1,28 @@
+const loadingBox = document.querySelector('#loading');
+const resultsBox = document.querySelector('#results');
+
 function getRecommendations() {
-    const loadingBox = document.querySelector('#loading');
     loadingBox.classList.remove('hidden');
+    resultsBox.classList.add('hidden');
+
+    const userID = document.querySelector('#user_id').value;
+
+    fetch(`/api/${userID}`)
+        .then(response => response.json())
+        .then(json => displayBooksList(json.predictions));
+
+
+}
+
+function displayBooksList(books) {
+    const ul = document.querySelector('.books-predictions');
+    ul.innerHTML = '';
+
+    books.forEach(book => {
+        const li = `<li>${book.title}</li>`;
+        ul.innerHTML += li;
+    });
+
+    loadingBox.classList.add('hidden');
+    resultsBox.classList.remove('hidden');
 }
